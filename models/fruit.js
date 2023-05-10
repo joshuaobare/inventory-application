ee61e7ff71b3ee7c69439505bc30584cb846dc8e
joshuaobare <jobare.obare@gmail.com>
2023-05-10 15:13:40 +0300

    created Fruit model and populated app.js

---
+++ models/fruit.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+const FruitSchema = new Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: Number, required: true },
+    quantity: { type: Number, required: true },
+    url: { type: String, required: true },
+})
+
+const Fruit = mongoose.model("Fruit", FruitSchema)
