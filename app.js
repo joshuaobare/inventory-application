15ac5a9492c74e6c777262b5757a6275b27f71db
joshuaobare <jobare.obare@gmail.com>
2023-05-14 20:50:23 +0300

    fixed error in app.js

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const compression = require('compression')
+const helmet = require('helmet')
 
 const indexRouter = require('./routes/index');
 
